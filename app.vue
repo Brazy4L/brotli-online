69c3dd076c104f61a83a49708099498327328881
Brazy <me@brazy.one>
2023-07-21 01:08:38 +0300

    Compress input

--- app.vue
+++ app.vue
@@ -1,7 +1,29 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { compress } from "brotli-compress";
+
+const inputText = ref("");
+
+async function getCompressedSizeInputText() {
+  const textEncoder = new TextEncoder();
+  const encodedInputText = textEncoder.encode(inputText.value);
+  const compressedInputText = await compress(encodedInputText);
+  const sizeInBytes = compressedInputText.length;
+  const sizeInKB = compressedInputText.length / 1_000;
+  const sizeInMB = compressedInputText.length / 1_000_000;
+  console.log(sizeInBytes);
+  console.log(sizeInKB);
+  console.log(sizeInMB);
+}
+</script>
 
 <template>
-  <div></div>
+  <textarea v-model="inputText" placeholder="/* Your Code Here */"></textarea>
+  <button @click="getCompressedSizeInputText">Compress</button>
 </template>
 
-<style></style>
+<style>
+body {
+  margin: 0;
+}
+</style>
